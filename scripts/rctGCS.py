8f9d47637ac5fb3e7962263fc5345e60abe931c0
Nathan Hui <ntlhui@gmail.com>
2020-02-15 21:32:06 -0800

    Added dicstrings

--- scripts/rctGCS.py
+++ scripts/rctGCS.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+###############################################################################
+#     Radio Collar Tracker Ground Control Software
+#     Copyright (C) 2020  Nathan Hui
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+# DATE        Name  Description
+# -----------------------------------------------------------------------------
+# 02/15/20    NH    Initial commit
+#
+###############################################################################
 
 import tkinter as tk
 import socket
@@ -15,14 +39,40 @@ from builtins import list
 
 
 class MAVModel:
+    '''
+    RCT Payload Model - This class should provide an object oriented view of the
+    vehicle state
+    '''
     class SDR_INIT_STATES(Enum):
+        '''
+        SDR Initialization States
+
+        find_devices - The system is searching for valid devices
+        wait_recycle - The system is cycling to retry initialization
+        usrp_prove   - The system is attempting to probe and initialize the URSP
+        rdy          - The system has initialized the USRP and is ready
+        fail         - The system has encountered a fatal error initializing the
+                       USRP
+        '''
         find_devices = 0
         wait_recycle = 1
         usrp_probe = 2
         rdy = 3
         fail = 4
 
-    class GPS_STATES(Enum):
+    class EXTS_STATES(Enum):
+        '''
+        GPS Initialization State
+
+        get_tty      - The system is opening the serial port
+        get_msg      - The system is waiting for a message from the external 
+                       sensors
+        wait_recycle - The system is cycling to retry initialization
+        rdy          - The system has initialized the external sensors and is 
+                       ready
+        fail         - The system has encountered a fatal error initializing the
+                       external sensors
+        '''
         get_tty = 0
         get_msg = 1
         wait_recycle = 2
@@ -30,6 +80,20 @@ class MAVModel:
         fail = 4
 
     class OUTPUT_DIR_STATES(Enum):
+        '''
+        Output Directory Initialization State
+
+        get_output_dir   - The system is obtaining the output directory path
+        check_output_dir - The system is checking that the output directory is
+                           located on removable media
+        check_space      - The system is checking that the external media has
+                           sufficient space
+        wait_recycle     - The system is cycling to retry initialization
+        rdy              - The system has initialized the storage location and
+                           is ready
+        fail             - The system has encountered a fatal error initializing
+                           the external storage
+        '''
         get_output_dir = 0
         check_output_dir = 1
         check_space = 2
@@ -38,6 +102,19 @@ class MAVModel:
         fail = 5
 
     class RCT_STATES(Enum):
+        '''
+        System Initialization State
+
+        init       - The system is starting all initialization threads
+        wait_init  - The system is waiting for initialization tasks to complete
+        wait_start - The system is initialized and is waiting for the start 
+                     command
+        start      - The system is starting the mission software
+        wait_end   - The system is running and waiting for the stop command
+        finish     - The system is stopping the mission software and ensuring
+                     data is written to disk
+        fail       - The system has encountered a fatal error
+        '''
         init = 0
         wait_init = 1
         wait_start = 2
@@ -47,11 +124,21 @@ class MAVModel:
         fail = 6
 
     class CALLBACK_EVENTS(Enum):
+        '''
+        Callback Events
+
+        Hearbeat  - Callback when a heartbeat message is received
+        Exception - Callback when an exception message is received
+        GetFreqs  - Callback when the payload broadcasts a set of frequencies
+        '''
         Heartbeat = 1,
         Exception = 2,
         GetFreqs = 3,
 
     def __init__(self):
+        '''
+        Creates the MAVModel object.
+        '''
         self.__log = logging.getLogger('rctGCS:MAVModel')
         self.__rx = MAVReceiver(9000)
         self.sdrStatus = 0
@@ -60,10 +147,10 @@ class MAVModel:
         self.sysStatus = 0
         self.swStatus = 0
         self.frequencies = []
-        self.__rx.registerRxCallback('heartbeat', self.processHeartbeat)
-        self.__rx.registerRxCallback('exception', self.handleRemoteException)
-        self.__rx.registerRxCallback('traceback', self.handleRemoteTraceback)
-        self.__rx.registerRxCallback('frequencies', self.processFrequencies)
+        self.__rx.registerRxCallback('heartbeat', self.__processHeartbeat)
+        self.__rx.registerRxCallback('exception', self.__handleRemoteException)
+        self.__rx.registerRxCallback('traceback', self.__handleRemoteTraceback)
+        self.__rx.registerRxCallback('frequencies', self.__processFrequencies)
         self.__exceptions = []
         self.lastException = [None, None]
         self.__log.info("MAVModel Created")
@@ -72,17 +159,33 @@ class MAVModel:
         for event in self.CALLBACK_EVENTS:
             self.__callbacks[event] = []
 
-    def start(self, gui=False):
-        self.__rx.start(gui)
+    def start(self, guiTickCallback=None):
+        '''
+        Initializes the MAVModel object
+        :param guiTickCallback: Callback to a function for progress bar
+        :type guiTickCallback: Function with no parameters to be called for 
+            progress
+        '''
+        self.__rx.start(guiTickCallback)
         self.__log.info("MVAModel started")
 
-    def processFrequencies(self, frequencies):
+    def __processFrequencies(self, frequencies):
+        '''
+        Internal callback to handle frequency messages
+        :param frequencies: Frequency message payload
+        :type frequencies: List of integers
+        '''
         self.__log.info("Received frequencies")
         self.frequencies = frequencies
         for callback in self.__callbacks[self.CALLBACK_EVENTS.GetFreqs]:
             callback()
 
-    def processHeartbeat(self, packet):
+    def __processHeartbeat(self, packet):
+        '''
+        Internal callback to handle heartbeat messages
+        :param packet: Heartbeat packet payload
+        :type packet:
+        '''
         self.__log.info("Received heartbeat")
         statusString = packet['status']
         self.sdrStatus = self.SDR_INIT_STATES(int(statusString[0]))
@@ -94,26 +197,54 @@ class MAVModel:
             callback()
 
     def stop(self):
+        '''
+        Stops the MAVModel and underlying resources
+        '''
         self.__rx.stop()
         self.__log.info("MVAModel stopped")
 
     def registerCallback(self, event: CALLBACK_EVENTS, callback):
+        '''
+        Registers a callback for the specified event
+        :param event: Event to trigger on
+        :type event: CALLBACK_EVENTS
+        :param callback: Callback to call
+        :type callback:
+        '''
         assert(isinstance(event, self.CALLBACK_EVENTS))
         self.__callbacks[event].append(callback)
 
     def startMission(self):
-        pass
+        '''
+        Sends the start mission command
+        '''
+        commandPacket = {'cmd': {'id': 'gcs', 'action': 'start'}}
+        self.__rx.sendMessage(commandPacket)
+        self.__log.info("Sent start command")
 
     def getFreqsFromRemote(self):
+        '''
+        Sends the command to retrieve frequencies
+        '''
         commandPacket = {'cmd': {'id': 'gcs', 'action': 'getF'}}
         self.__rx.sendMessage(commandPacket)
         self.__log.info("Sent getF command")
 
-    def handleRemoteException(self, exception):
+    def __handleRemoteException(self, exception):
+        '''
+        Internal callback to handle exception messages
+        :param exception: Exception string
+        :type exception:
+        '''
         self.__log.exception("Remote Exception: %s" % exception)
         self.lastException[0] = exception
 
-    def handleRemoteTraceback(self, traceback):
+    def __handleRemoteTraceback(self, traceback):
+        '''
+        Internal callback to handle traceback messages
+        :param traceback: Traceback string
+        :type traceback:
+        '''
         self.__log.exception("Remote Traceback: %s" % traceback)
         self.lastException[1] = traceback
 #         This is a hack - there is no guarantee that the traceback occurs after
@@ -122,9 +253,15 @@ class MAVModel:
             callback()
 
     def setFrequencies(self, freqs):
+        '''
+        Sends the command to set the specified frequencies
+        :param freqs:
+        :type freqs:
+        '''
         assert(freqs, list)
         for freq in freqs:
             assert(isinstance(freq, int))
+        # TODO: finish
 
 
 class MAVReceiver:
@@ -152,7 +289,7 @@ class MAVReceiver:
         self.__mavIP = None
         self.__lastHeartbeat = None
         self.__packetMap = {
-            'heartbeat': [self.processHeartbeat],
+            'heartbeat': [self.__processHeartbeat],
             '_nheartbeat': [],
             'exception': [],
         }
@@ -181,9 +318,6 @@ class MAVReceiver:
                 guiTick()
         self.__log.error("Failed to receive any heartbeats")
         return (None, None)
-
-    def processPing(self):
-        pass
 
     def __receiverLoop(self):
         '''
@@ -236,16 +370,33 @@ class MAVReceiver:
         self.__log.info('RCT MAVReceiver stopped')
         self.sock.close()
 
-    def processHeartbeat(self, packet):
+    def __processHeartbeat(self, packet):
+        '''
+        Internal callback to handle recognizing loss of heartbeat
+        :param packet:
+        :type packet:
+        '''
         self.__lastHeartbeat = dt.datetime.now()
 
     def registerRxCallback(self, packetKey, callback):
+        '''
+        Registers a callback for the particular packet keyword
+        :param packetKey: Keyword to recognize
+        :type packetKey: String
+        :param callback: Callback function
+        :type callback: function pointer
+        '''
         if packetKey in self.__packetMap:
             self.__packetMap[packetKey].append(callback)
         else:
             self.__packetMap[packetKey] = [callback]
 
     def sendMessage(self, packet: dict):
+        '''
+        Sends the specified dictionary as a packet
+        :param packet: Packet to send
+        :type packet: dictionary
+        '''
         assert(isinstance(packet, dict))
         msg = json.dumps(packet)
         self.__log.info("Send: %s" % (msg))
@@ -253,31 +404,45 @@ class MAVReceiver:
 
 
 class GCS(tk.Tk):
+    '''
+    Ground Control Station GUI
+    '''
+
     def __init__(self):
+        '''
+        Creates the GCS Application Object
+        '''
         super().__init__()
         self.__log = logging.getLogger('rctGCS.GCS')
         self.__mavModel = MAVModel()
         self.__buttons = []
         self.innerFreqFrame = None
         self.freqElements = []
-        self.createWidgets()
+        self.__createWidgets()
         for button in self.__buttons:
             button.config(state='disabled')
         self.__mavModel.registerCallback(
-            self.__mavModel.CALLBACK_EVENTS.Heartbeat, self.updateStatus)
+            self.__mavModel.CALLBACK_EVENTS.Heartbeat, self.__updateStatus)
         self.__mavModel.registerCallback(
-            self.__mavModel.CALLBACK_EVENTS.Exception, self.handleRemoteException)
+            self.__mavModel.CALLBACK_EVENTS.Exception, self.__handleRemoteException)
         self.__mavModel.registerCallback(
-            self.__mavModel.CALLBACK_EVENTS.GetFreqs, self.setFreqsFromRemote)
+            self.__mavModel.CALLBACK_EVENTS.GetFreqs, self.__setFreqsFromRemote)
 
     def start(self):
+        '''
+        Start thread for the GCSP Application object.  This is responsible for
+        initializing the comm channel.
+        '''
         self.progressBar['maximum'] = 30
         try:
             self.__mavModel.start(self.progressBar.step)
         except RuntimeError:
-            self.noHeartbeat()
+            self.__noHeartbeat()
 
-    def setFreqsFromRemote(self):
+    def __setFreqsFromRemote(self):
+        '''
+        Callback to update the GUI with frequencies from the payload
+        '''
         self.__log.info("Setting frequencies")
         freqs = self.__mavModel.frequencies
         if self.innerFreqFrame is not None:
@@ -294,52 +459,84 @@ class GCS(tk.Tk):
         self.update()
 
     def mainloop(self, n=0):
+        '''
+        Main Application Loop
+        :param n:
+        :type n:
+        '''
         self.__startThread = threading.Thread(target=self.start)
         self.__startThread.start()
         tk.Tk.mainloop(self, n=n)
 
-    def startCommand(self):
-        pass
+    def __startCommand(self):
+        '''
+        Internal callback to send the start command
+        '''
 
-    def stopCommand(self):
-        pass
+    def __stopCommand(self):
+        '''
+        Internal callback to send the stop command
+        '''
 
-    def getFreqs(self):
+    def __getFreqs(self):
+        '''
+        Internal callback to retrieve the frequencies from the remote
+        '''
         self.__mavModel.getFreqsFromRemote()
-        pass
 
-    def addFreq(self):
+    def __addFreq(self):
+        '''
+        Internal callback to add a new frequency entry
+        '''
         self.freqElements.append(tk.Entry(self.innerFreqFrame))
         self.freqElements[-1].pack()
         self.innerFreqFrame.update()
         self.__log.info("Added frequency entry")
 
-    def removeFreq(self):
+    def __removeFreq(self):
+        '''
+        Internal callback to remove the lowest frequency entry
+        '''
         self.freqElements[-1].destroy()
         self.freqElements.remove(self.freqElements[-1])
         self.innerFreqFrame.update()
         self.__log.info("Removed last frequency entry")
 
-    def sendFreq(self):
-        pass
+    def __sendFreq(self):
+        '''
+        Internal callback to send the current set of frequencies
+        '''
 
-    def configureOpts(self):
-        pass
+    def __configureOpts(self):
+        '''
+        Internal callback to open the option configuration window
+        '''
 
-    def upgradeSoftware(self):
-        pass
+    def __upgradeSoftware(self):
+        '''
+        Internal callback to start the firmware upgrade process
+        '''
 
-    def noHeartbeat(self):
+    def __noHeartbeat(self):
+        '''
+        Internal callback for the no heartbeat state
+        '''
         for button in self.__buttons:
             button.config(state='disabled')
         tkm.showerror(
             title="RCT GCS", message="No Heartbeats Received")
 
-    def handleRemoteException(self):
+    def __handleRemoteException(self):
+        '''
+        Internal callback for an exception message
+        '''
         tkm.showerror(title='RCT GCS', message='An exception has occured!\n%s\n%s' % (
             self.__mavModel.lastException[0], self.__mavModel.lastException[1]))
 
-    def updateStatus(self):
+    def __updateStatus(self):
+        '''
+        Internal callback for status variable update
+        '''
         for button in self.__buttons:
             button.config(state='normal')
         self.progressBar['value'] = 0
@@ -414,22 +611,28 @@ class GCS(tk.Tk):
         else:
             self.swStatusLabel.config(text='SW: NULL', bg='red')
 
-    def windowClose(self):
+    def __windowClose(self):
+        '''
+        Internal callback for window close
+        '''
         self.__startThread.join(timeout=1)
         self.__mavModel.stop()
         self.destroy()
         self.quit()
 
-    def createWidgets(self):
+    def __createWidgets(self):
+        '''
+        Internal helper to make GUI widgets
+        '''
         self.title('RCT GCS')
         self.grid_columnconfigure(0, weight=1)
         self.startButton = tk.Button(
-            self, text='Start', command=self.startCommand)
+            self, text='Start', command=self.__startCommand)
         self.startButton.grid(row=0, column=0, sticky='we')
         self.__buttons.append(self.startButton)
 
         self.stopButton = tk.Button(
-            self, text='Stop', command=self.stopCommand)
+            self, text='Stop', command=self.__stopCommand)
         self.stopButton.grid(row=1, column=0, sticky='we')
         self.__buttons.append(self.stopButton)
 
@@ -440,32 +643,32 @@ class GCS(tk.Tk):
         self.freqFrame.grid(row=2, column=0, sticky='we')
 
         self.getFrequencyButton = tk.Button(
-            self, text='Get Frequencies', command=self.getFreqs)
+            self, text='Get Frequencies', command=self.__getFreqs)
         self.getFrequencyButton.grid(row=3, column=0, sticky='we')
         self.__buttons.append(self.getFrequencyButton)
 
         self.addFreqButton = tk.Button(
-            self, text='Add Frequency', command=self.addFreq)
+            self, text='Add Frequency', command=self.__addFreq)
         self.addFreqButton.grid(row=4, column=0, sticky='we')
         self.__buttons.append(self.addFreqButton)
 
         self.removeFreqButton = tk.Button(
-            self, text='Remove Frequency', command=self.removeFreq)
+            self, text='Remove Frequency', command=self.__removeFreq)
         self.removeFreqButton.grid(row=5, column=0, sticky='we')
         self.__buttons.append(self.removeFreqButton)
 
         self.commitFreqButton = tk.Button(
-            self, text="Upload Frequencies", command=self.sendFreq)
+            self, text="Upload Frequencies", command=self.__sendFreq)
         self.commitFreqButton.grid(row=6, column=0, sticky='we')
         self.__buttons.append(self.commitFreqButton)
 
         self.configureButton = tk.Button(
-            self, text="Configure", command=self.configureOpts)
+            self, text="Configure", command=self.__configureOpts)
         self.configureButton.grid(row=7, column=0, sticky='we')
         self.__buttons.append(self.configureButton)
 
         self.upgradeButton = tk.Button(
-            self, text="Upgrade Software", command=self.upgradeSoftware)
+            self, text="Upgrade Software", command=self.__upgradeSoftware)
         self.upgradeButton.grid(row=8, column=0, sticky='we')
         self.__buttons.append(self.upgradeButton)
 
@@ -488,7 +691,7 @@ class GCS(tk.Tk):
         self.swStatusLabel = tk.Label(self.statusFrame, text="SW: NULL")
         self.swStatusLabel.grid(row=5, column=1)
 
-        self.protocol("WM_DELETE_WINDOW", self.windowClose)
+        self.protocol("WM_DELETE_WINDOW", self.__windowClose)
         self.progressBar = ttk.Progressbar(
             self, orient='horizontal', mode='determinate')
         self.progressBar.grid(row=10, column=0, sticky='we')
