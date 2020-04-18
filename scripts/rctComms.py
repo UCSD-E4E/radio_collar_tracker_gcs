8c24ecb7ec5cd0e8b277d281bf5d71bd620a0142
Nathan Hui <ntlhui@gmail.com>
2020-04-17 18:30:04 -0700

    Moved commands and events enums to module scope, added helper for sending commands, cleaned up eventing mechanisms

--- scripts/rctComms.py
+++ scripts/rctComms.py
@@ -20,6 +20,8 @@
 #
 # DATE        Name  Description
 # -----------------------------------------------------------------------------
+# 04/16/20    NH    Moved Commands and Events to module scope, added helper for
+#                   sending commands, cleaned up eventing mechanisms
 # 04/14/20    NH    Initial commit, fixed start parameters, added support for
 #                   multiline packet
 #
@@ -30,6 +32,50 @@ import json
 import logging
 import datetime as dt
 import threading
+import enum
+
+from time import sleep
+
+
+class COMMANDS(enum.Enum):
+    GET_FREQUENCY = "getF"
+    START = 'start'
+    STOP = 'stop'
+    SET_FREQUENCY = 'setF'
+    GET_OPTIONS = 'getOpts'
+    SET_OPTIONS = 'setOpts'
+    WRITE_OPTIONS = 'writeOpts'
+    UPGRADE = 'upgrade'
+
+
+class PACKET_TYPES(enum.Enum):
+    HEARTBEAT = 'heartbeat'
+    PING = 'ping'
+    FREQUENCIES = 'frequencies'
+    EXCEPTION = 'exception'
+    TRACEBACK = 'traceback'
+    OPTIONS = 'options'
+    UPGRADE_READY = 'upgrade_ready'
+    UPGRADE_STATUS = 'upgrade_status'
+    UPGRADE_COMPLETE = 'upgrade_complete'
+    COMMAND = 'cmd'
+
+
+class EVENTS(enum.Enum):
+    HEARTBEAT = PACKET_TYPES.HEARTBEAT
+    PING = PACKET_TYPES.PING
+    FREQUENCIES = PACKET_TYPES.FREQUENCIES
+    EXCEPTION = PACKET_TYPES.EXCEPTION
+    TRACEBACK = PACKET_TYPES.TRACEBACK
+    OPTIONS = PACKET_TYPES.OPTIONS
+    UPGRADE_READY = PACKET_TYPES.UPGRADE_READY
+    UPGRADE_STATUS = PACKET_TYPES.UPGRADE_STATUS
+    UPGRADE_COMPLETE = PACKET_TYPES.UPGRADE_COMPLETE
+    COMMAND = PACKET_TYPES.COMMAND
+    NO_HEARTBEAT = enum.auto()
+    UNKNOWN_PACKET = enum.auto()
+    GENERAL_EXCEPTION = enum.auto()
+
 
 class MAVReceiver:
     '''
@@ -37,7 +83,7 @@ class MAVReceiver:
     '''
     __BUFFER_LEN = 1024
 
-    def __init__(self, port: int):
+    def __init__(self, port: int, originString='gcs'):
         '''
         Initializes the UDP interface on the specified port.  Also specifies a
         filename to use as a logfile, which defaults to no log.
@@ -56,10 +102,12 @@ class MAVReceiver:
         self.__mavIP = None
         self.__lastHeartbeat = None
         self.__packetMap = {
-            'heartbeat': [self.__processHeartbeat],
-            '_nheartbeat': [],
-            'exception': [],
+            EVENTS.HEARTBEAT: [self.__processHeartbeat],
+            EVENTS.NO_HEARTBEAT: [],
+            EVENTS.GENERAL_EXCEPTION: [],
+            EVENTS.UNKNOWN_PACKET: [],
         }
+        self.__originString = originString
 
     def waitForHeartbeat(self, guiTick=None, timeout: int=30):
         '''
@@ -78,9 +126,8 @@ class MAVReceiver:
                 data, addr = self.sock.recvfrom(1024)
                 strData = data.decode('utf-8')
                 for line in strData.split('\r\n'):
-                    print(line)
                     packet = json.loads(line)
-                    if 'heartbeat' in packet:
+                    if PACKET_TYPES.HEARTBEAT.value in packet:
                         self.__log.info("Received heartbeat %s" % (packet))
                         self.__lastHeartbeat = dt.datetime.now()
                         return addr, packet
@@ -94,20 +141,31 @@ class MAVReceiver:
         Receiver thread
         '''
         self.__log.info('RCT MAVReceiver rxThread started')
+        keywordEventMap = {}
+        for event in EVENTS:
+            if isinstance(event.value, PACKET_TYPES):
+                keywordEventMap[event.value.value] = event
         while self.__run:
             ready = select.select([self.sock], [], [], 1)
             if ready[0]:
                 data, addr = self.sock.recvfrom(self.__BUFFER_LEN)
                 self.__log.info("Received: %s" % data.decode())
                 packet = json.loads(data.decode())
-                for key in self.__packetMap.keys():
-                    if key in packet:
-                        for callback in self.__packetMap[key]:
-                            callback(packet[key])
+                for key in packet.keys():
+                    try:
+                        event = keywordEventMap[key]
+                        for callback in self.__packetMap[event]:
+                            callback(packet=packet[key], addr=addr)
+                    except KeyError:
+                        for callback in self.__packetMap[EVENTS.UNKNOWN_PACKET]:
+                            callback(packet=packet, addr=addr)
+                    except Exception:
+                        for callback in self.__packetMap[EVENTS.GENERAL_EXCEPTION]:
+                            callback(packet=packet, addr=addr)
             if (dt.datetime.now() - self.__lastHeartbeat).total_seconds() > 30:
-                self.__log.warn("No heartbeats!")
-                for callback in self.__packetMap['_nheartbeat']:
-                    callback(None)
+                self.__log.warning("No heartbeats!")
+                for callback in self.__packetMap[EVENTS.NO_HEARTBEAT]:
+                    callback(packet=None, addr=None)
 
     def start(self, gui=None):
         '''
@@ -140,26 +198,27 @@ class MAVReceiver:
         self.__log.info('RCT MAVReceiver stopped')
         self.sock.close()
 
-    def __processHeartbeat(self, packet):
+    def __processHeartbeat(self, **args):
         '''
         Internal callback to handle recognizing loss of heartbeat
-        :param packet:
-        :type packet:
         '''
         self.__lastHeartbeat = dt.datetime.now()
 
-    def registerRxCallback(self, packetKey, callback):
+    def registerCallback(self, event: EVENTS, callback):
         '''
         Registers a callback for the particular packet keyword
-        :param packetKey: Keyword to recognize
-        :type packetKey: String
+        :param event: Event to trigger on
+        :type event: EVENTS
         :param callback: Callback function
-        :type callback: function pointer
+        :type callback: function pointer.  The function shall accept two 
+                keyword parameters: packet (dict) and addr (str).  The packet
+                dictionary shall the the packet payload, and the addr shall be
+                the address of the MAV
         '''
-        if packetKey in self.__packetMap:
-            self.__packetMap[packetKey].append(callback)
+        if event in self.__packetMap:
+            self.__packetMap[event].append(callback)
         else:
-            self.__packetMap[packetKey] = [callback]
+            self.__packetMap[event] = [callback]
 
     def sendMessage(self, packet: dict):
         '''
@@ -172,3 +231,28 @@ class MAVReceiver:
         self.__log.info("Send: %s" % (msg))
         self.sock.sendto(msg.encode('utf-8'), self.__mavIP)
 
+    def sendCommandPacket(self, command: COMMANDS, options: dict = None):
+        '''
+        Generates a generic command packet
+        :param command:    Command to generate
+        :type command: COMMANDS
+        :param options: Options for command
+        :type options: dict
+        :return dict    Populated generic command packet
+        '''
+        packet = {}
+        commandPacket = {}
+        commandPacket['id'] = self.__originString
+        commandPacket['action'] = command.value
+        if options is not None:
+            commandPacket.update(options)
+        packet[PACKET_TYPES.COMMAND.value] = commandPacket
+        self.sendMessage(packet)
+
+
+if __name__ == '__main__':
+    rx = MAVReceiver(9000)
+    rx.start()
+    sleep(1)
+    rx.sendCommandPacket(COMMANDS.START)
+    rx.stop()
