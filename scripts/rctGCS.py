1325f4bb14e541b9aad8f6337e1bb233b8ff3bf6
Mia Lucio <mlucio@ucsd.edu>
2020-07-09 02:29:50 -0700

    changelog fixed

--- scripts/rctGCS.py
+++ scripts/rctGCS.py
@@ -1,4 +1,52 @@
+#!/usr/bin/env python3
+###############################################################################
+#     Radio Collar Tracker Ground Control Software
+#     Copyright (C) 2020  Nathan Hui
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+# DATE      WHO Description
+# -----------------------------------------------------------------------------
+# 06/30/20  ML  Translated tkinter GUI into PyQt5
+# 06/17/20  ML  Implemented ability to load webmap from OSM based on coordinates
+# 05/29/20  ML  refactored MapControl 
+# 05/25/20  NH  Fixed validate frequency call
+# 05/24/20  ML  Implemented ability to load map, refactored map functions
+# 05/24/20  AG  Added error messages during target frequency validation.
+# 05/20/20  NH  Fixed window close action, added function to handle registering
+#                 callbacks when connection established, removed unused
+#                 callbacks, added advanced settings dialog, fixed logging
+# 05/19/20  NH  Removed PIL, rasterio, refactored options, connectionDialog,
+#                 AddTargetDialog, SystemSettingsControl, fixed exit behavior,
+# 05/18/20  NH  Updated API for core
+# 05/17/20  AG  Finished implementing start/stop recording button
+# 05/17/20  AG  Added setting target frequencies and system connection text updates.
+# 05/13/20  AG  Added ability to set and receive expert debug options
+# 05/09/20  ML  Added ability to add/clear target frequencies
+# 05/05/20  AG  Tied options entries to string vars
+# 05/03/20  ML  Added Expert Settings popup, Added the ability to load TIFF img
+# 05/03/20  AG  Added TCP connection and update options functionalities
+# 04/26/20  NH  Updated API, switched from UDP to TCP
+# 04/20/20  NH  Updated API and imports
+# 04/17/20  NH  Updated imports and MAVModel API
+# 02/15/20  NH  Initial commit
+#
+###############################################################################
 import datetime as dt
+import math
 import time
 import logging
 import sys
@@ -12,6 +60,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5 import QtNetwork
 
 class GCS(QMainWindow):
     '''
@@ -439,7 +488,7 @@ class SystemSettingsControl(CollapseFrame):
             self.optionVars["SDR_centerFreq"].setText(str(cntrFreq))
             self.optionVars["SDR_samplingFreq"].setText(str(sampFreq))
             self.frm_targHolder.setVerticalSpacing(0)
-            time.sleep(1)
+            #time.sleep(0.5)
             for freq in self.__root._mavModel.getFrequencies(self.__root.defaultTimeout):
                 #Put in frm_targHolder
                 new = QHBoxLayout()
@@ -943,8 +992,13 @@ class MapControl(CollapseFrame):
         btn_loadWebMap = QPushButton('Load') 
         btn_loadWebMap.clicked.connect(lambda:self.__loadWebMap())
         lay_loadWebMap.addWidget(btn_loadWebMap, 3, 1)
+
+        btn_cacheMap = QPushButton('Cache Map') 
+        btn_cacheMap.clicked.connect(lambda:self.__cacheMap())
+        lay_loadWebMap.addWidget(btn_cacheMap, 4, 1)
         controlPanel.addWidget(frm_loadWebMap)
         controlPanel.addLayout(lay_loadWebMap)
+
 
         '''
         #TODO: Move Map options and map legend to separate control class
@@ -993,6 +1047,10 @@ class MapControl(CollapseFrame):
         self.__mapFrame.setParent(None)
         self.__mapFrame = StaticMap(self.__holder)
 
+    def __cacheMap(self):
+        lat = float(self.__latEntry.text())
+        lon = float(self.__lonEntry.text())
+        self.__mapFrame.cacheMap(lat,lon)
 
 
 
@@ -1049,6 +1107,8 @@ class StaticMap(QWidget):
     Helper Class to facilititate displaying online web maps
 '''
 class WebMap(QWidget):
+    DEBUG_PORT = '5588'
+    DEBUG_URL = 'http://127.0.0.1:%s' % DEBUG_PORT
 
     def __init__(self, root, lat, lon, zoom):
         # Initialize WebMapFrame
@@ -1067,10 +1127,69 @@ class WebMap(QWidget):
 
         self.setLayout(holder)
         root.addWidget(self, 0, 1, 1, 2)
+        self.root = root
+        prof = QWebEngineProfile.defaultProfile()
+        prof.setCachePath("./webcache2")
+        prof.setPersistentStoragePath("./webcache2")
+        #self.web.page().profile().clearHttpCache()
+
+
+
+
+    def cacheMap(self, lat, lng):
+        LDN_COORDINATES = (lat, lng)
+        #urlstr = "./webcache/index.html"
+
+        self.deg2num(lat, lng, 10)
+
+        urlstr = 'https://a.tile.openstreetmap.org/10/' +str(self.x)+ '/' +str(self.y)+ '.png'
+
+        print(urlstr)
+        
+        url = QUrl(urlstr)
+        req = QtNetwork.QNetworkRequest(url)
+        man = QtNetwork.QNetworkAccessManager()
+
+        cache = QtNetwork.QNetworkDiskCache()
+        cache.setCacheDirectory("./webcache")
+        man.setCache(cache)
+
+        def handleResponse(reply):
+
+            er = reply.error()
+
+            if er == QtNetwork.QNetworkReply.NoError:
+
+                bytes_string = reply.readAll()
+                print(str(bytes_string, 'utf-8'))
+
+            else:
+                print("Error occured: ", er)
+                print(reply.errorString())
+
+
+            data = reply.readAll()
+            im = QImage()
+            im.loadFromData(data)
+            im.save('new.png', "PNG");
+
+        man.finished.connect(handleResponse)
+        man.get(req)
+
+
+    def deg2num(self, lat_deg, lon_deg, zoom):
+        lat_rad = math.radians(lat_deg)
+        n = 2.0 ** zoom
+        self.x = int((lon_deg + 180.0) / 360.0 * n)
+        self.y = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+        
+
 
 
     def __buildMap(self, LDN_COORDINATES, zoom):
         folmap = folium.Map(location=LDN_COORDINATES, zoom_start=zoom)
+        pop = folium.LatLngPopup()
+        folmap.add_child(pop)
         self.html_string = folmap.get_root().render()
         self.web.setHtml(self.html_string)
 
