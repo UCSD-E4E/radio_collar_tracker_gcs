911564abf23b00a719124b8b5b3599f00f2db11c
Nathan Hui <ntlhui@gmail.com>
2020-08-06 23:14:26 -0700

    Fixed syntax and variable masking

--- scripts/rctCore.py
+++ scripts/rctCore.py
@@ -20,6 +20,7 @@
 #
 # DATE      WHO Description
 # -----------------------------------------------------------------------------
+# 08/06/20  NH  Fixed syntax and variable masking
 # 07/30/20  NH  Updated rctCore.MAVModel docstrings
 # 05/25/20  NH  Added docstring to new events, added callback to process pings,
 #                 added type hint to setFrequencies
@@ -579,8 +580,8 @@ class MAVModel:
             packet: Ping packet
             addr: Source address
         '''
-        ping = ping.rctPing.fromPacket(packet)
-        estimate = self.EST_mgr.addPing(ping)
+        pingObj = ping.rctPing.fromPacket(packet)
+        estimate = self.EST_mgr.addPing(pingObj)
         for callback in self.__callbacks[Events.NewPing]:
             callback()
         if estimate is not None:
@@ -594,7 +595,7 @@ class MAVModel:
             packet:    Vehicle Position packet
             addr:    Source address
         '''
-        coordinate = [packet.lat, packet.lon. packet.alt, packet.hdg]
+        coordinate = [packet.lat, packet.lon, packet.alt, packet.hdg]
         self.state['VCL_track'][packet.timestamp] = coordinate
         for callback in self.__callbacks[Events.VehicleInfo]:
             callback()
