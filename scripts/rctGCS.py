457e6a18a37028375c73c53d1d553b15b23f6876
yahmad3495 <yahmad@ucsd.edu>
2020-12-17 11:04:45 -0800

    WIP: Polygon Map tool

--- scripts/rctGCS.py
+++ scripts/rctGCS.py
@@ -74,6 +74,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import QtNetwork
+from PyQt5.Qt import QSvgWidget
 #import qgis
 from qgis.core import *    
 from qgis.gui import *  
@@ -1686,7 +1687,11 @@ class PolygonMapTool(QgsMapToolEmitPoint):
         self.startPoint = self.toMapCoordinates(e.pos())
         self.endPoint = self.startPoint
         self.isEmittingPoint = True
+        self.addVertex(self.startPoint, self.canvas)
         self.showLine(self.startPoint, self.endPoint)
+
+    
+        
 
     def canvasReleaseEvent(self, e):
         self.isEmittingPoint = False
@@ -1696,9 +1701,31 @@ class PolygonMapTool(QgsMapToolEmitPoint):
             return
 
         self.endPoint = self.toMapCoordinates(e.pos())
+        self.showLine(self.startPoint, self.endPoint)
+
+    def addVertex(self, selectPoint, canvas):
+        m1 = QgsVertexMarker(canvas)
+        
+        print(selectPoint)
+        m1.setCenter(QgsPointXY(selectPoint))
+        m1.setColor(QColor(255,0, 0)) #(R,G,B)
+        m1.setIconSize(5)
+        m1.setIconType(QgsVertexMarker.ICON_X)
+        m1.setPenWidth(3)
+
+        point1 = QgsPointXY(selectPoint)
+        m1.show() 
 
     def showLine(self, startPoint, endPoint):
-        return
+        self.rubberBand.reset(QgsWkbTypes.PolygonGeometry)
+        if startPoint.x() == endPoint.x() or startPoint.y() == endPoint.y():
+            return
+        
+        point1 = QgsPointXY(startPoint.x(), startPoint.y())
+
+        self.rubberBand.addPoint(point1, True)
+
+        self.rubberBand.show()
 
     def deactivate(self):
         QgsMapTool.deactivate(self)
@@ -2148,9 +2175,8 @@ class MapOptions(QWidget):
         '''
         Function to add Map Legend widget when map is loaded
         '''
-        #TODO add labels and symbols for vehicle, pings, and vehicle path
-        curLayout = self.layout()
-        #curLayout.addWidget(legendWidget)
+        mapLegend = MapLegend()
+        self.layout().addWidget(mapLegend)
 
     
     def exportPing(self):
@@ -2624,6 +2650,105 @@ class StaticMap(MapWidget):
             print('valid layer')
         else:
             print('invalid layer')
+
+class MapLegend(QWidget):
+    '''
+    Custom widget to display map legend
+    '''
+    def __init__(self):
+            QWidget.__init__(self)
+
+            self.__createWidget()
+
+    def __createWidget(self):
+        '''
+        Helper function that creates layout of legend
+        '''
+
+        self.__layMapLegend = QGridLayout()
+        self.setLayout(self.__layMapLegend)
+        self.__layMapLegend.setSpacing(10)
+
+        #Creating the labels for legend
+        lbl_vehicleSymb = QLabel('Vehicle Location', self)
+        lbl_vehicleTravel = QLabel('Vehicle Path', self)
+        lbl_symbolBlue = QLabel('Weakest', self)
+        lbl_symbolGreen = QLabel('Weak', self)
+        lbl_symbolYellow = QLabel('Neutral', self)
+        lbl_symbolOrange = QLabel('Strong', self)
+        lbl_symbolRed = QLabel('Strongest', self)
+        lbl_estimatePoint = QLabel('Estimated point', self)
+
+
+        #Adds vehicle locations icon
+        img_vehicleSymb = QSvgWidget('../resources/vehicleSymbol.svg', self)
+        img_vehicleSymb.load('../resources/vehicleSymbol.svg')
+        img_vehicleSymb.setFixedWidth(50)
+        img_vehicleSymb.setFixedHeight(50)
+
+        #Adding weakest ping
+        img_symbolBlue = QSvgWidget('../resources/symbBlue.svg', self)
+        img_symbolBlue.load('../resources/symbBlue.svg')
+        img_symbolBlue.setFixedWidth(50)
+        img_symbolBlue.setFixedHeight(50)
+
+        #Adding weak ping
+        img_symbolGreen = QSvgWidget('../resources/symbGreen.svg', self)
+        img_symbolGreen.load('../resources/symbGreen.svg')
+        img_symbolGreen.setFixedWidth(50)
+        img_symbolGreen.setFixedHeight(50)
+
+        #Adding Neutral ping
+        img_symbolYellow = QSvgWidget('../resources/symbYellow.svg', self)
+        img_symbolYellow.load('../resources/symbYellow.svg')
+        img_symbolYellow.setFixedWidth(50)
+        img_symbolYellow.setFixedHeight(50)
+
+        #Adding Strong ping
+        img_symbolOrange = QSvgWidget('../resources/symbOrange.svg', self)
+        img_symbolOrange.load('../resources/symbOrange.svg')
+        img_symbolOrange.setFixedWidth(50)
+        img_symbolOrange.setFixedHeight(50)           
+
+        #Adding Strongest ping
+        img_symbolRed = QSvgWidget('../resources/symbRed.svg', self)
+        img_symbolRed.load('../resources/symbRed.svg')
+        img_symbolRed.setFixedWidth(50)
+        img_symbolRed.setFixedHeight(50)
+
+        #Adding Estimated Point
+        img_symbolEstPoint = QSvgWidget('../resources/symbEstPoint.svg', self)
+        img_symbolEstPoint.load('../resources/symbEstPoint.svg')
+        img_symbolEstPoint.setFixedWidth(50)
+        img_symbolEstPoint.setFixedHeight(50)
+
+        #Adding Vehicle Path
+        img_symbolVPath = QSvgWidget('../resources/symbVehiclePath.svg', self)
+        img_symbolVPath.load('../resources/symbVehiclePath.svg')
+        img_symbolVPath.setFixedWidth(50)
+        img_symbolVPath.setFixedHeight(50)
+
+
+        #Adding Labels to the layout
+        self.__layMapLegend.addWidget(lbl_estimatePoint, 1, 1)
+        self.__layMapLegend.addWidget(lbl_vehicleSymb, 2, 1)
+        self.__layMapLegend.addWidget(lbl_vehicleTravel, 3, 1)
+        self.__layMapLegend.addWidget(lbl_symbolBlue, 4, 1)
+        self.__layMapLegend.addWidget(lbl_symbolGreen, 5, 1)
+        self.__layMapLegend.addWidget(lbl_symbolYellow, 6, 1)
+        self.__layMapLegend.addWidget(lbl_symbolOrange, 7, 1)
+        self.__layMapLegend.addWidget(lbl_symbolRed, 8, 1)
+
+        #Adding label images to the layout
+        self.__layMapLegend.addWidget(img_symbolEstPoint, 1, 2)
+        self.__layMapLegend.addWidget(img_vehicleSymb,2,2)
+        self.__layMapLegend.addWidget(img_symbolVPath,3,2)
+        self.__layMapLegend.addWidget(img_symbolBlue,4,2)
+        self.__layMapLegend.addWidget(img_symbolGreen,5,2)
+        self.__layMapLegend.addWidget(img_symbolYellow,6,2)
+        self.__layMapLegend.addWidget(img_symbolOrange, 7, 2)
+        self.__layMapLegend.addWidget(img_symbolRed,8,2)
+
 
 def configSetup():
     '''
