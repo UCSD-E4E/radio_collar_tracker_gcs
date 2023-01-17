f6072a50de05d7bfe3863e7f33764f923c6f9209
hgrehm <hgrehm@ucsd.edu>
2023-01-17 15:57:03 -0800

    fixing merge

--- scripts/ui/display.py
+++ scripts/ui/display.py
@@ -42,8 +42,6 @@ class GCS(QMainWindow):
         '''
         super().__init__()
         self.__log = logging.getLogger('rctGCS.GCS')
-        with open('gcsconfig.json') as handle:
-            self.options = json.load(handle)
         self.portVal = self.defaultPortVal
         self._transport = None
         self._mavModels = {}
