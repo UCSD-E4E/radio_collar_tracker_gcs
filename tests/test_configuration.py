3bf9b34a04912338cbbb5ccec2ba5dee2fa669e8
Nathan Hui <nthui@eng.ucsd.edu>
2023-02-28 10:55:05 -0800

    Added configuration test

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -4,17 +4,17 @@ import os
 import sys
 from pathlib import Path
 
+from RctGcs.config import get_instance
+
 if 'CONDA_PREFIX' in os.environ:
     sys.path.insert(0, Path(sys.executable).parent.joinpath("Library", "python", "plugins").as_posix())
     sys.path.insert(0, Path(sys.executable).parent.joinpath("Library", "python").as_posix())
 from qgis.core import QgsApplication, QgsStyle
 
-# from scripts.config import get_instance
 
-
-# def test_configuration():
-#     with get_instance(Path('gcsConfig.ini')) as config:
-#         prefix_path = config.qgis_prefix_path
-#     QgsApplication.setPrefixPath(str(prefix_path), True)
-#     default_styles = QgsStyle.defaultStyle()
-#     assert len(default_styles.colorRampNames()) > 0
+def test_configuration():
+    with get_instance(Path('gcsConfig.ini')) as config:
+        prefix_path = config.qgis_prefix_path
+    QgsApplication.setPrefixPath(str(prefix_path), True)
+    default_styles = QgsStyle.defaultStyle()
+    assert len(default_styles.colorRampNames()) > 0
