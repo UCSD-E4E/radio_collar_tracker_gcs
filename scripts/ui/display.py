69b595376bb77671de00c4ca5b37c74717e2e9e2
Wesleylewis05 <wjl4jj@virginia.edu>
2023-03-11 14:30:06 -0500

    fixed multiple client issue

--- scripts/ui/display.py
+++ scripts/ui/display.py
@@ -112,6 +112,7 @@ class GCS(QMainWindow):
                 try:
                     self._transport = RCTTCPClient(addr=self.addr_val, port=self.port_val)
                     self.connection_handler(self._transport, 0)
+                    return
                 except ConnectionRefusedError:
                     self.user_popups.show_timed_warning(text="Trying to reconnect. Attempt {} out of 5.\
                                                         \nRetrying after {} seconds.".format(str(i), retry_time), timeout=retry_time)
